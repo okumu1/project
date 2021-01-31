bdf55b78f8ec0f92412da03d38483fbb4dd6f036
okumu <arnold.jokumu@gmail.com>
2021-01-31 17:04:32 +0300

    main app_assignment planner

---
+++ sys_dev/app/src/main/java/com/example/assignmentplanner/RegisterActivity.java
@@ -0,0 +1,14 @@
+package com.example.assignmentplanner;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class RegistraterActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_registrater);
+    }
+}
